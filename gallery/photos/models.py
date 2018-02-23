bce811223ed24bc9fad4462d061f3eee8fd3ceec
evantoh <evanmwenda@gmail.com>
2018-02-23 15:50:43 +0300

    add class location to store the location detail and class category to define different categories of the images

--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -2,10 +2,26 @@ from django.db import models
 
 # Create model image.
 class Image(models.Model):
-    Image = models.CharField(max_length=30)
+    Image_Description = models.CharField(max_length=30,null=True)
     Image_Name = models.CharField(max_length=30)
-    Image = models.CharField(max_length=30)
+    # Image = models.CharField(max_length=30)
+    # Image_Location=models.ForeignKey(Location,blank =True)
+    # Image_Category=models.ManyToManyField(Category,blank =True)
+    Image_date=models.DateTimeField(auto_now_add=True,null=True)
 
+
+# create model location
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
     
     
 
