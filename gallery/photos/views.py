efa39fd3e8b9d21f53241fd6542abe489caf0d67
evantoh <evanmwenda@gmail.com>
2018-02-23 11:33:01 +0300

    add the template to my app to separate views and templates for easy debugging

--- gallery/photos/views.py
+++ gallery/photos/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+
+from django .shortcuts import render 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome to personal gallery')
+    return render(request,'welcome.html')
