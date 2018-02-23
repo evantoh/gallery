22bc29f2ddb97c477b0828e1f1ff7bdc95f046cf
evantoh <evanmwenda@gmail.com>
2018-02-23 17:13:51 +0300

    add test to test deletion of images

--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -30,3 +30,6 @@ class Image(models.Model):
     def save_image(self):
         self.save()
 
+    def delete_image(self):
+        self.delete()
+
--- gallery/photos/tests.py
+++ gallery/photos/tests.py
@@ -6,14 +6,26 @@ from .models import Image,Category,Location
 class imageTestClass(TestCase):
     # set up method
     def setUp(self):
-        self.image=Image(Image_Description = 'the beaty nature',Image_Name='travel')
+        self.image=Image(Image_Description = 'the beaty nature',
+        Image_Name='travel')
 
         # testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.image,Image))
 
-# testing save method
+# testing save method for image
     def test_save_method(self):
         self.image.save_image()
         images= Image.objects.all()
         self.assertTrue(len(images)> 0)
+
+# testing delete image
+    def test_delete_image(self):
+        self.image=Image(Image_Description = 'the beaty nature',
+        Image_Name='travel')
+
+        self.image.delete_image()
+        images= Image.objects.all()
+        self.assertTrue(len(images)< 1)
+
+
