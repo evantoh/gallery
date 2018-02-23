5eb40ef4cfef1a0a4b4e1f741301f8159e16a61a
evantoh <evanmwenda@gmail.com>
2018-02-23 16:37:37 +0300

    add first test to test instantiaon of the class Image

--- gallery/photos/tests.py
+++ gallery/photos/tests.py
@@ -3,7 +3,11 @@ from .models import Image,Category,Location
 
 #create test for image class
 
-# class imageTestClass(TestCase):
-#     # set up method
-#     def setUp(self):
-#         self.
+class imageTestClass(TestCase):
+    # set up method
+    def setUp(self):
+        self.image=Image(Image_Description = 'the beaty nature',Image_Name='travel')
+
+        # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
